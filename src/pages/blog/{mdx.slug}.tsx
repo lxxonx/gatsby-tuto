eb8da3ba6b68211eecd2b2a670013a06e014e0cb
lxxonx <leeonechang92@gmail.com>
2022-03-28 14:55:13 +0900

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -2,6 +2,8 @@ import { graphql } from "gatsby";
 import * as React from "react";
 import Layout from "../../components/layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { FileNode } from "gatsby-plugin-image/dist/src/components/hooks";
 
 type BlogPostProps = {
   data: {
@@ -9,6 +11,10 @@ type BlogPostProps = {
       frontmatter: {
         title: string;
         date: string;
+        hero_image_alt: string;
+        hero_image_credit_link: string;
+        hero_image_credit_text: string;
+        hero_image: FileNode;
       };
       body: string;
     };
@@ -16,9 +22,18 @@ type BlogPostProps = {
 };
 
 const BlogPost: React.FC<BlogPostProps> = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image!} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -30,6 +45,14 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
